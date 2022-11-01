d3a211e117c10503403a331921ace5c4b4e5df71
a-dilmen <113839940+a-dilmen@users.noreply.github.com>
2022-11-01 10:03:14 +0300

    CodeForces 263A A. Beautiful Matrix solution

--- ABeautifulMatrix.java
+++ ABeautifulMatrix.java
@@ -1,17 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ABeautifulMatrix {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// A Beautiful Matrix - 263A		
 		//Scanner Object
 		Scanner sc = new Scanner(System.in);
 		//distance x & y axis
-		int l = 0;	
-		int k = 0;	
+		int x = 0;	
+		int y = 0;	
 		//input 5v5 matrix 0's and one 1
 		for(int i = 0; i<5;i++) {
 			//input read as string
@@ -19,31 +16,24 @@ public class ABeautifulMatrix {
 			//check for whether 1 is in string
 			if (a.contains("1")){
 				//y distance
-				k = i;
+				y = i;
 				//Casting String to integer
 				String[] arr = a.split(" ");
 				//seeking for x distance
 				for(int j = 0;j<5;j++) {
 					if (Integer.valueOf(arr[j]) == 1) {
-						l = j;
+						x = j;
 					}
 				}
 			}
-		}
-		//initializing number of moves
-		int summ = 0;
+		}	
 		//checking distance to center
-		if (k<=2) {
-			summ = summ+2-k;
-		}else 
-			summ = summ+k-2;
-		
-		if (l<=2) {
-			summ = summ+2-l;
-		}else 
-			summ = summ+l-2;
-		//printing result
-		System.out.println(summ);
+		int xDistance=Math.abs(x-2);
+		int yDistance=Math.abs(y-2);
+		int tDistance = xDistance+yDistance;
+		System.out.println(tDistance);
+		System.out.println(y+""+yDistance);
+		sc.close();
 	}
 
 }
